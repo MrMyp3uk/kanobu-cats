2076135b4895e8db13eafa5021b0f9c888bc32e2
Mr. Myp3uk <naxe@mail.ru>
2020-10-06 17:48:41 +0300

    Changed sticker selection behaviour. Now stickers can be sent along with the message text

--- cats.js
+++ cats.js
@@ -4,31 +4,34 @@
     let domNode = document.querySelector('section.skeleton');
     if (domNode === null) return;
 
-    const EDITOR_SELECTOR = 'div.c-editor';
-    const EDITOR_BUTTONS_BAR_SELECTOR = 'div.c-editor_options:not(.has-stickers)';
-    const STICKER_MARK_CLASS = 'has-stickers';
-    const SHOUT_SELECTOR = '.js-list-item';
+    const ZERO_WIDTH_WHITESPACE = '​';
 
-    const BUTTON_TEMPLATE_HTML = `
+    const BUTTON_TEMPLATE = htmlToTemplate(`
 <div style="margin-left: 5px; height: 24px; width: 36px; background-color: #eee; border-radius: 3px; cursor: pointer;">
-    <svg height="24" viewBox="0 -32 512 512" width="36" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 -32 512 512" xmlns="http://www.w3.org/2000/svg" style="width: 36px; height: 24px;">
         <path d="m342.382812 239.351562c-23.039062-35.941406-62.277343-57.402343-104.964843-57.402343-42.683594 0-81.925781 21.460937-104.960938 57.402343l-55.515625 86.605469c-9.210937 14.371094-13.460937 30.96875-12.292968 47.996094 1.167968 17.03125 7.648437 32.890625 18.738281 45.871094 11.097656 12.976562 25.761719 21.84375 42.40625 25.648437 16.644531 3.800782 33.707031 2.179688 49.339843-4.691406l1.019532-.453125c39.339844-16.957031 84.304687-16.804687 123.546875.453125 10.121093 4.449219 20.84375 6.699219 31.664062 6.699219 5.882813 0 11.800781-.667969 17.664063-2.003907 16.644531-3.800781 31.308594-12.667968 42.410156-25.644531 11.09375-12.976562 17.578125-28.839843 18.75-45.871093 1.171875-17.035157-3.078125-33.632813-12.289062-48.007813zm26.246094 160.972657c-14.121094 16.507812-36.964844 21.726562-56.847656 12.984375-23.632812-10.394532-49-15.589844-74.375-15.589844-25.351562 0-50.714844 5.191406-74.332031 15.574219l-.671875.296875c-19.730469 8.34375-42.238282 3.058594-56.203125-13.265625-14.105469-16.511719-15.710938-39.886719-3.992188-58.171875l55.519531-86.605469c17.492188-27.289063 47.28125-43.582031 79.691407-43.582031 32.410156 0 62.203125 16.292968 79.699219 43.582031l55.511718 86.601563c11.722656 18.292968 10.113282 41.671874-4 58.175781zm0 0" />
         <path d="m91.894531 239.238281c16.515625-6.34375 29.0625-19.652343 35.332031-37.476562 5.960938-16.960938 5.472657-36.109375-1.382812-53.921875-6.859375-17.800782-19.335938-32.332032-35.128906-40.921875-16.597656-9.019531-34.828125-10.488281-51.316406-4.132813-33.171876 12.753906-48.394532 53.746094-33.929688 91.398438 11.554688 29.96875 38.503906 48.886718 65.75 48.886718 6.957031 0 13.933594-1.234374 20.675781-3.832031zm-58.417969-55.835937c-8.523437-22.1875-1.035156-45.789063 16.703126-52.609375 3.203124-1.234375 6.589843-1.847657 10.046874-1.847657 5.335938 0 10.847657 1.457032 16.152344 4.34375 9.539063 5.183594 17.160156 14.183594 21.457032 25.335938 4.292968 11.160156 4.675781 22.941406 1.074218 33.179688-3.300781 9.382812-9.617187 16.28125-17.78125 19.417968l-.015625.007813c-17.714843 6.828125-39.085937-5.660157-47.636719-27.828125zm0 0" />
         <path d="m199.613281 171.386719c41.46875 0 75.207031-38.4375 75.207031-85.683594 0-47.257813-33.738281-85.703125-75.207031-85.703125-41.464843 0-75.199219 38.445312-75.199219 85.703125 0 47.246094 33.734376 85.683594 75.199219 85.683594zm0-141.375c24.917969 0 45.195313 24.984375 45.195313 55.691406 0 30.695313-20.277344 55.671875-45.195313 55.671875s-45.1875-24.976562-45.1875-55.671875c0-30.707031 20.269531-55.691406 45.1875-55.691406zm0 0" />
         <path d="m329.496094 192.4375h.003906c6.378906 2.117188 12.886719 3.128906 19.367188 3.128906 30.242187 0 59.714843-22.011718 70.960937-55.839844 6.476563-19.472656 6.050781-40.0625-1.199219-57.972656-7.585937-18.746094-21.644531-32.355468-39.589844-38.324218-17.945312-5.960938-37.363281-3.476563-54.664062 7-16.527344 10.011718-29.191406 26.246093-35.65625 45.71875-13.652344 41.078124 4.640625 84.273437 40.777344 96.289062zm-12.296875-86.824219c4.222656-12.714843 12.292969-23.191406 22.726562-29.511719 9.652344-5.847656 20.183594-7.335937 29.648438-4.191406 9.460937 3.148438 17 10.640625 21.234375 21.101563 4.574218 11.304687 4.769531 24.53125.539062 37.246093-8.433594 25.375-31.933594 40.492188-52.382812 33.699219-20.433594-6.796875-30.199219-32.96875-21.765625-58.34375zm0 0" />
         <path d="m487.875 182.4375-.011719-.011719c-28.597656-21.125-71.367187-11.96875-95.347656 20.421875-23.957031 32.40625-20.210937 75.972656 8.34375 97.113282 10.414063 7.714843 22.71875 11.402343 35.3125 11.402343 21.949219 0 44.785156-11.203125 60.046875-31.804687 23.957031-32.40625 20.214844-75.972656-8.34375-97.121094zm-15.777344 79.265625c-14.160156 19.113281-38.101562 25.453125-53.378906 14.136719-15.265625-11.300782-16.195312-36.042969-2.074219-55.144532 9.386719-12.679687 23.097657-19.734374 35.734375-19.734374 6.390625 0 12.507813 1.804687 17.648438 5.605468 15.253906 11.3125 16.179687 36.046875 2.070312 55.136719zm0 0" />
     </svg>
-</div>`;
+</div>`);
 
-    const PANEL_TEMPLATE_HTML = `
+    const PICKER_TEMPLATE = htmlToTemplate(`
 <div style="background-color: #2b303b; border: 1px solid #4f535c; margin-top: 10px; overflow-y: scroll; max-height: 225px;">
     <div class="cat-stickers" style="display: flex; flex-wrap: wrap; margin-bottom: 5px;"></div>
-</div>`;
+</div>`);
 
-    const STICKER_TEMPLATE_HTML = `
+    const STICKER_TEMPLATE = htmlToTemplate(`
 <div class="cat-sticker" style="margin: 5px; cursor: pointer; width: 64px; height: 64px; text-align: center;">
-    <img class="cat-sticker-img" style="max-height: 100%; max-width: 100%;">
-</div>`;
+    <img class="cat-sticker-img" style="max-height: 100%; max-width: 100%;" alt="Sticker">
+</div>`);
+
+    const STICKER_PREVIEW_TEMPLATE = htmlToTemplate(`
+<div style="margin-top: 10px; display: none; padding: 5px; border: 1px dashed #eee;">
+    <img alt="Preview" src="">
+    <span style="display: inline-block; margin-left: 5px; color: #eee; font-size: 16px; cursor: pointer;">✖<span>
+</div>`);
 
     const STICKER_URLS = [
         'https://u.kanobu.ru/cries/2020/10/03/0df51b7d-6b5a-48c3-92f2-d27d2f52542b.jpg',
@@ -147,100 +150,164 @@
         'https://u.kanobu.ru/cries/2020/10/04/bedcb8f5-6c20-4319-9944-3aa06838b32a.jpg'
     ];
 
-    const BUTTON_TEMPLATE = htmlToElement(BUTTON_TEMPLATE_HTML);
+    const STICKER_PICKER = new class StickerPicker {
+        constructor() {
+            this.root = PICKER_TEMPLATE.cloneNode(true);
 
-    const stickerPanel = createStickersPanel();
-
-    function catify() {
-        const buttonsBars = document.querySelectorAll(EDITOR_BUTTONS_BAR_SELECTOR);
-        for (buttonsBar of buttonsBars) {
-            buttonsBar.classList.add(STICKER_MARK_CLASS);
-            addStickersButton(buttonsBar);
-        }
-    }
-
-    function addStickersButton(buttonsBar) {
-        const button = BUTTON_TEMPLATE.cloneNode(true);
-        button.addEventListener('click', event => {
-            event.preventDefault();
-            const editor = button.closest(EDITOR_SELECTOR);
-            if (editor) {
-                if (editor.contains(stickerPanel)) {
-                    editor.removeChild(stickerPanel);
-                } else {
-                    editor.appendChild(stickerPanel);
-                }
+            const stickerList = this.root.querySelector('.cat-stickers');
+            for (let url of STICKER_URLS) {
+                let sticker = STICKER_TEMPLATE.cloneNode(true);
+                sticker.querySelector('.cat-sticker-img').src = url;
+                stickerList.appendChild(sticker);
             }
-        });
-        buttonsBar.appendChild(button);
-    }
 
-    function createStickersPanel() {
-        const panel = htmlToElement(PANEL_TEMPLATE_HTML);
-        const stickers = panel.querySelector('.cat-stickers');
-        const stickerTemplate = htmlToElement(STICKER_TEMPLATE_HTML);
-        for (let url of STICKER_URLS) {
-            let sticker = stickerTemplate.cloneNode(true);
-            sticker.querySelector('.cat-sticker-img').src = url;
-            stickers.appendChild(sticker);
-        }
-
-        stickers.addEventListener('click', event => {
-            if (!event.target.classList.contains('cat-sticker-img')) return;
-            event.preventDefault();
-            event.stopPropagation();
-            sendSticker(event.target.src, getTargetPostUrl(panel));
-            panel.remove();
-        });
-
-        return panel;
-    }
-
-    function sendSticker(stickerUrl, targetPostUrl) {
-        fetch(stickerUrl)
-            .then(response => response.blob())
-            .then(blob => {
-                const data = new FormData();
-                data.append('image', blob, 'image.png');
-
-                return fetch(targetPostUrl,
-                {
-                    method: 'post',
-                    body: data,
-                    credentials: 'include',
-                    mode: 'cors',
-                    headers: {
-                        'X-CSRFToken': getCookieValue('csrftoken')
-                    }
-                });
+            stickerList.addEventListener('click', event => {
+                if (!event.target.classList.contains('cat-sticker-img')) return;
+                event.preventDefault();
+                event.stopPropagation();
+                if (this.onStickerSelected) {
+                    this.onStickerSelected(event.target.src);
+                }
             });
-    }
+        }
+    };
 
-    function getTargetPostUrl(panel) {
-        const shout = panel.closest(SHOUT_SELECTOR);
-        if (shout === null) {
-            return 'https://kanobu.ru/api/v1/cries/';
+    class ShoutEditor {
+        constructor(element) {
+            element.classList.add('has-stickers');
+            this.root = element;
+            this.isAnswer = element.closest('.js-list-item') !== null;
+            this.text = element.querySelector('.ql-editor');
+
+            const button = BUTTON_TEMPLATE.cloneNode(true);
+            button.addEventListener('click', event => {
+                event.preventDefault();
+                this.togglePicker();
+            });
+            element.querySelector('.c-editor_options').appendChild(button);
+
+            const preview = new StickerPreview();
+            element.querySelector('.quill').appendChild(preview.root);
+            this.preview = preview;
         }
 
-        let shoutId = shout.querySelector('a[href^="/shouts/"]').href.match(/\/shouts\/(\d+)\//)[1];
-        return `https://kanobu.ru/api/v1/cries/${shoutId}/answers/`;
+        togglePicker() {
+            if (this.root.contains(STICKER_PICKER.root)) {
+                this.detachPicker();
+            } else {
+                this.attachPicker();
+            }
+        }
+
+        attachPicker() {
+            STICKER_PICKER.onStickerSelected = url => {
+                if (this.text.classList.contains('ql-blank')) {
+                    this.text.textContent = ZERO_WIDTH_WHITESPACE;
+                }
+                this.preview.show(url);
+            }
+            this.root.appendChild(STICKER_PICKER.root);
+        }
+
+        detachPicker() {
+            STICKER_PICKER.onStickerSelected = null;
+            this.root.removeChild(STICKER_PICKER.root);
+        }
     }
 
-    function htmlToElement(html) {
+    class StickerPreview {
+        constructor() {
+            this.root = STICKER_PREVIEW_TEMPLATE.cloneNode(true);
+            this.image = this.root.querySelector('img');
+            this.root.querySelector('span').addEventListener('click', event => {
+                event.preventDefault();
+                this.hide();
+            });
+        }
+
+        show(url) {
+            this.image.src = url;
+            this.root.style.display = 'inline-block';
+        }
+
+        hide() {
+            this.image.src = '';
+            this.root.style.display = 'none';
+        }
+    }
+
+    function htmlToTemplate(html) {
         const template = document.createElement('template');
         template.innerHTML = html.trim();
         return template.content.firstChild;
     }
 
-    function getCookieValue(name) {
-        let matches = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
-        return matches ? matches.pop() : '';
+    function catify() {
+        const editors = document.querySelectorAll('.c-editor:not(.has-stickers)');
+        for (let editor of editors) {
+            let shoutEditor = new ShoutEditor(editor);
+            if (shoutEditor.isAnswer) {
+                window.__stickers.editors.answer = shoutEditor;
+            } else {
+                window.__stickers.editors.newShout = shoutEditor;
+            }
+        }
     }
 
-    const observerOptions = { childList: true, subtree: true };
+    function interceptAjaxRequests() {
+        const originalOpen = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function (...args) {
+            let [method, url] = args;
+            if (method.toLowerCase() === 'post') {
+                if (/(?:https:\/\/kanobu.ru)?\/api\/v1\/cries\/\d+\/answers\/?/.test(url)) {
+                    this.__stickerEditor = window.__stickers.editors.answer;
+                } else if (/(?:https:\/\/kanobu.ru)?\/api\/v1\/cries\/?/i.test(url)) {
+                    this.__stickerEditor = window.__stickers.editors.newShout;
+                }
+            }
+
+            originalOpen.apply(this, args);
+        };
+
+        const originalSend = XMLHttpRequest.prototype.send;
+        XMLHttpRequest.prototype.send = function (...args) {
+            if (this.__stickerEditor && this.__stickerEditor.preview.image.src) {
+                let formData = args[0];
+                if (formData instanceof FormData) {
+                    fetch(this.__stickerEditor.preview.image.src)
+                        .then(response => response.blob())
+                        .then(blob => {
+                            formData.set('image', blob, 'sticker.png');
+                            const originalOnLoad = this.onload;
+                            this.onload = () => {
+                                this.__stickerEditor.preview.hide();
+                                if (originalOnLoad) {
+                                    originalOnLoad.call(this);
+                                }
+                            };
+
+                            originalSend.apply(this, args);
+                        })
+                        .catch(() => this.abort());
+                    
+                    return;
+                }
+            }
+    
+            originalSend.apply(this, args);
+        };
+    }
+
     const observer = new MutationObserver(catify);
-    observer.observe(domNode, observerOptions);
+    window.__stickers = {
+        observer,
+        editors: {
+            newShout: null,
+            answer: null
+        }
+    };
 
     catify();
-    window.__stickers = { observer };
+    observer.observe(domNode, { childList: true, subtree: true });
+    interceptAjaxRequests();
 })();
